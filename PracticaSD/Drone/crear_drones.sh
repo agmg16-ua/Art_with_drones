dfd255d4280d9f27d093347a89a3f037fe316e24
agmg16-ua <agmg16@alu.ua.es>
2023-11-06 18:38:44 +0100

    Changed ip

--- PracticaSD/Drone/crear_drones.sh
+++ PracticaSD/Drone/crear_drones.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ip_engine=192.168.1.84
-ip_broker=192.168.1.84
-ip_registry=192.168.1.84
+ip_engine=192.168.0.35
+ip_broker=192.168.0.35
+ip_registry=192.168.0.35
 tiempo=5
 
 gnome-terminal -- bash -c "python3 AD_Drone.py drone1 $ip_engine 9090 $ip_broker 9092 $ip_registry 8081; exec bash"
