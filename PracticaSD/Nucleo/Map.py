d013f1709ec213b6f711c843ade950fa70330f48
agmg16-ua <agmg16@alu.ua.es>
2023-11-07 16:44:36 +0100

    Commented Map

--- PracticaSD/Nucleo/Map.py
+++ PracticaSD/Nucleo/Map.py
@@ -3,6 +3,7 @@ import sys
 import os
 import copy
 
+"""
 class Drone:
     def _init_(self, _id):
         self.id = _id
@@ -16,44 +17,59 @@ class Drone:
 
     def set_pos_final(self, pos_final):
         self.pos_final = pos_final
+"""
 
-
+#Clase para generar el mapa
 class Map:
+    #Inicia el tamaño del mapa y el string que contiene el mapa a vacio
     def _init_(self):
         self.filas = 19
         self.columnas = 19
         self.mapa = ""
 
+    #Devuelve las filas del mapa
     def get_filas(self):
         return self.filas
 
+    #Devuelve las columnas del mapa
     def get_columnas(self):
         return self.columnas
 
+    #Método principal para generar el mapa
+    #Recibe las posiciones finales de los drones (drones) y los drones que hay en el sistema (dronesActuales)
     def print_mapa(self, drones, dronesActuales):
         rojo = "\u001B[91m"
         verde = "\u001B[32m"
         reset = "\u001B[0m"
+
+        #Recorre el mapa
         for i in range(0, 21):
             for j in range(0, 21):
+
+                #Si es la primera fila escribe los numeros de las columnas
                 if i == 0:
                     if j == 0:
                         self.mapa += "     1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20 "
+
+                #Si es la primera columna escribe los numeros de las filas
                 elif j == 0:
                     if i < 10:
                         self.mapa += f" {i} "
                     else:
                         self.mapa += f"{i} "
+                
+                #Si no escribe el mapa
                 else:
-                    existe = False
+                    existe = False #Variable para saber si en esa posición hay un drone
                     for droneActual in dronesActuales:
                         droneFinal = []
 
-                        #Encontrar posicion final del drone
+                        #Encontrar posicion final del drone y se guarda para acceder más fácilmente
                         for drone in drones:
                             if drone[0] == droneActual[0]:
                                 droneFinal = drone
 
+                        #Si hay un drone en esa posición se escribe en el mapa
                         if droneActual[1][1] == i and droneActual[1][0] == j:
                             existe = True
                             if droneActual[0] < 10:
@@ -67,17 +83,25 @@ class Map:
                                 else:
                                     self.mapa += f"{rojo}{droneActual[0]}{reset} "
                             break
+                    
+                    #Si no hay un drone en esa posición se escribe un espacio vacio
                     if existe == False:
                         self.mapa += "   "
 
+                #Se deja un espacio entre las columnas
                 self.mapa += " "
+            
+            #Se deja un espacio entre las filas
             self.mapa += "\n\n"
 
+    #Método para convertir los drones en un mapa en formato string
     def to_string(self, drones, dronesActuales):
-        dronesAux = drones.copy()
+        dronesAux = drones.copy() #Copia las posiciones finales para no modificar el original
+
         if len(dronesActuales) == 0:
             dronesAux = [[0, [0, 0]]]
         
+        #Si no existe el drone en la figura (posiciones finales) actual se añade la posicion de la base
         for actual in dronesActuales:
             existe = False
             for buscando in dronesAux:
@@ -90,12 +114,13 @@ class Map:
         #print(dronesAux)
         #print(dronesActuales)
 
+        #Se llama al método para generar el mapa
         self.mapa = ""
         sys.stdout.write("\b")
         self.print_mapa(dronesAux, dronesActuales)
         return self.mapa
 
-
+#Método para limpiar la terminal
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
