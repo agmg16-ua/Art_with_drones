49449e8707a2728000d35845ec02b73e30b01448
agmg16-ua <agmg16@alu.ua.es>
2023-12-16 11:43:15 +0100

    Cambio de posicion de la api y el front

--- PracticaSD/Nucleo/REST_SD/appSD.js
+++ PracticaSD/Nucleo/REST_SD/appSD.js
@@ -20,7 +20,7 @@ const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
 
 // Configuración de la conexión a la base de datos SQLite
-const dbPath = "./registry"; // Ruta al archivo de la base de datos SQLite
+const dbPath = "./../registry"; // Ruta al archivo de la base de datos SQLite
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error("Error al abrir la base de datos", err.message);
