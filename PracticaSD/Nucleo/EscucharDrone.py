78572e0581b97442b8dc7994ef04ecfe88a853f0
Victor <vpg66@gcloud.ua.es>
2024-01-02 12:57:28 +0100

    Auditoria en EscucharDrones

--- PracticaSD/Nucleo/EscucharDrone.py
+++ PracticaSD/Nucleo/EscucharDrone.py
@@ -1,6 +1,22 @@
 import socket
 import threading
 import sqlite3
+#Libreria para auditoria
+from loguru import logger
+
+# Obtener la dirección IP de la máquina
+ip_address = socket.gethostbyname(socket.gethostname())
+
+# Configurar el sistema de registro con el formato personalizado
+logger.add('auditoria.log', level='INFO', format="{time} {level} - Acción: {function} - IP: {ip} - Descripción: {message}")
+
+# Decorador para asignar un Logger con IP a la función
+def logger_decorator(func):
+    def wrapper(*args, **kwargs):
+        loguru_logger = logger.bind(function=func.__name__, ip=ip_address)
+        func.logger = loguru_logger
+        return func(*args, **kwargs)
+    return wrapper
 
 #Realiza todas las operaciones posibles para autenticar a un drone
 class EscucharDrone(threading.Thread):
@@ -10,26 +26,34 @@ class EscucharDrone(threading.Thread):
         self.drone = skDrone
 
     #Lee del socket de forma controlada
+    @logger_decorator
     def lee_socket(self, p_datos):
+        self.lee_socket.logger.info("Leyendo socket")
         try:
             aux = self.drone.recv(1024)
             p_datos = aux.decode()
         except Exception as e:
+            self.lee_socket.logger.error(f"Error leyendo socket: {e}")
             print(f"Error leyendo socket: {e}")
         return p_datos
 
     #Escribe en el socket de forma controlada
+    @logger_decorator
     def escribe_socket(self, p_datos):
+        self.escribe_socket.logger.info("Escribiendo socket")
         try:
             self.drone.send(p_datos.encode())
         except Exception as e:
+            self.escribe_socket.logger.error(f"Error escribiendo socket: {e}")
             print(f"Error escribiendo socket: {e}")
 
     #Autentica al drone conectado en el espectaculo.
     #Comprueba que el token y la id del drone son las correctas en mi base de datos.
     #Devuelve un booleano diciendo si se ha autenticado y un string que tendrá la id
     #para que el drone se vea en el espectaculo. En caso de que no se una obtiene un string vacio
+    @logger_decorator
     def autenticar(self, token, id):
+        self.autenticar.logger.info("Autenticando drone")
         try:
             """
             with open("drones.txt", "r") as archivo:
@@ -54,13 +78,16 @@ class EscucharDrone(threading.Thread):
                     return True, id_virtual_drone
 
         except Exception as e:
+            self.autenticar.logger.error(f"Error autenticando al drone: {e}")   
             print(f"Error autenticando al drone: {e}")
             return False,""
         return False,""
 
     #Codigo que se ejecutará al hacer .start
     #Devuelve al drone si ha sido aceptado o denegado
+    @logger_decorator
     def run(self):
+        self.run.logger.info("Contactando drone")
         print("Contactando drone...")
         token_id = ""
         existe = False
@@ -75,6 +102,8 @@ class EscucharDrone(threading.Thread):
                 self.escribe_socket("denegado")
 
         except Exception as e:
+            self.run.logger.error(f"Error en escucharDrone: {e}")
             print(f"Error en escucharDrone: {e}")
         finally:
+            self.run.logger.info("Cerrando socket")
             self.drone.close()
